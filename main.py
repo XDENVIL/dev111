619474aabd35e4a93950874095ea1507ebec7508
iamnoob937 <83622041+iamnoob937@users.noreply.github.com>
2021-08-30 17:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client as Bot
-from .config import API_ID, API_HASH, BOT_TOKEN
-from .videoplayer import app
+from config import API_ID, API_HASH, BOT_TOKEN
+from videoplayer import app
 
 bot = Bot(
     ":memory:",
