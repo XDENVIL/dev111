c1dd60288d42e9119c94c8e7c663b23d8e890823
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-08-31 22:43:21 +0530

    Update start.py

--- player/start.py
+++ player/start.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Hey I am Innexia Radio Bot 📻\n\n** `Add this Bot to your Group and Make it Admin \n2) Add` @InnexiaRadioBot `to your Group` \n3) **Commands** : \n`/radio`ytlink Live \n`/stop`",   
+       await m.reply(f"**Hey I am Innexia Radio Bot 📻\n\n** \n`Add this Bot to your Group and Make it Admin \n2) Add` @InnexiaRadioBot `to your Group` \n3) **Commands** : \n`/radio` Ytlink Live \n`/stop`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
